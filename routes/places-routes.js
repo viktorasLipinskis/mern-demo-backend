79edef19da13cccac4aa243d9dd86267453081f1
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2020-07-08 03:16:33 +0000

    wip

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const placesControllers = require("../controllres/places-controller");
+const placesControllers = require("../controllers/places-controller");
 
 router.get("/:pid", placesControllers.getPlaceById);
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
