9aab7c10558ee4b146ee00c04cd7e31a3f87f782
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2020-07-08 04:43:37 +0000

    implemented geocoding api usage

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -2,6 +2,7 @@ const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../utils/location");
 
 let DUMMY_PLACES = [
   {
@@ -40,13 +41,22 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ user });
 };
 
-const createPlace = (req, res, next) => {
-  const { title, description, coordinates, address, creator } = req.body;
+const createPlace = async (req, res, next) => {
+  const { title, description, address, creator } = req.body;
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
     console.log(errors);
-    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+    return next(
+      new HttpError("Invalid inputs passed, please check your data.", 422)
+    );
+  }
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
   }
 
   const createdPlace = {
