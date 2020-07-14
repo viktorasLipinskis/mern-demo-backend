bf6a83c07cbbd7c95d128cdf113dc183df09a1e1
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2020-07-14 04:49:14 +0000

    added cors issue fix

--- app.js
+++ app.js
@@ -10,6 +10,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+//This is needed to add to every api request for CORS issues on frontend
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use((error, req, res, next) => {
   if (res.headerSent) {
     return next(error);
