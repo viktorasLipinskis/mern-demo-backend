52c7aa35854517aaa335cd15712a7d1aabb6de07
Viktoras Lipinskis <viktoras.lipinskis@gmail.com>
2020-07-26 08:55:12 +0000

    wipo

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -120,6 +120,11 @@ const updatePlaceById = async (req, res, next) => {
     throw new HttpError("Invalid inputs passed, please check your data.", 422);
   }
 
+  if (place.creator.toString() !== req.userData.userId) {
+    const error = new HttpError("You are not allowed to edit this place.", 401);
+    return next(error);
+  }
+
   let place;
   try {
     place = await Place.findById(placeId);
